7f08ba80bf2ae5ea6842163233a7d5eb36503ed9
ddock4you <ddock4you@gmail.com>
2020-06-26 12:00:26 +0900

    - day11 주석 추가 및 문제 푼 링크 추가

--- day11/index.js
+++ day11/index.js
@@ -2,12 +2,30 @@ import express from "express";
 import movieRouter from "./movieRouter";
 
 const app = express();
-const localMiddleware = (req, res, next) => {
-    res.locals.siteName = "Nomad Movies";
-    next();
-};
 
 app.set("view engine", "pug");
 app.use("/", localMiddleware, movieRouter);
 
 app.listen(3000, () => console.log("hello"));
+
+/*
+    함수 설명
+    - getMovies returns an array of movies, console.log the result to see the shape of the object.
+    - getMovieById returns a movie object. It requires an ID as an argument and if no movie is found it returns undefined.
+    - getMovieByMinimumRating returns an array of movies, it requires a number, with this number the function will return an array of movies with a rating equal or higher than the number.
+    - getMovieByMinimumYear returns an array of movies, it requires a number, with this number the function will return an array of movies with a release date equal or higher than the number.
+
+    조건
+    - Complete all the controllers
+    - Use pug and mixins to render pages and loop over the movies list.
+    - Use templates.
+    - You can only have 3 .pug pages. movies.pug, 404.pug, detail.pug
+    - Use middlewares to add locals like siteTitle
+    - / should render movies.pug and show a list of movies.
+    - /:id should render the detail of a movie
+    - /filter should filter by year and rating.
+    - The app must behave exactly like the video
+    - IF ANY OF THE REQUIREMENTS ARE NOT FULFILLED YOU WILL GET AN ❌
+
+    https://codesandbox.io/s/express-controller-blueprint-23w25
+*/
